89209aa216d1573defd8e1000cf47c946c0c30a5
Aditya Casturi <aditya.casturi@gmail.com>
2025-03-30 18:46:26 -0700

    Removed old getData method

--- src/main/java/DatabaseExplorer.java
+++ src/main/java/DatabaseExplorer.java
@@ -1,7 +1,5 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
 import model.ColumnSchema;
 import model.ColumnSchemaTypeAdapter;
 import model.TableSchema;
@@ -12,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 class DatabaseExplorer {
-    // List all databases by listing directories inside the "dbs" folder.
     public static List<String> getDatabases() {
         List<String> databases = new ArrayList<>();
         File dbsFolder = new File(Constants.STORAGE_LOC);
@@ -48,7 +45,6 @@ class DatabaseExplorer {
         return tables;
     }
 
-    // Read and return the schema for a given table.
     public static TableSchema getTableSchema(String dbName, String tableName) throws Exception {
         String schemaFilePath = String.format(Constants.SCHEMA_FILE_LOC, dbName, tableName);
         File schemaFile = new File(schemaFilePath);
@@ -63,26 +59,5 @@ class DatabaseExplorer {
                 .create();
 
         return gson.fromJson(schemaFileJson, TableSchema.class);
-    }
-
-    // Read and return the data for a given table.
-    public static List<String[]> getTableData(String dbName, String tableName) throws Exception {
-        String dataFilePath = Constants.STORAGE_LOC + File.separator + dbName + File.separator + tableName + ".data";
-        File dataFile = new File(dataFilePath);
-        if (!dataFile.exists()) {
-            throw new Exception("Data file does not exist");
-        }
-
-        List<String[]> rows = new ArrayList<>();
-        try (CSVReader csvReader = new CSVReader(new FileReader(dataFile))) {
-            String[] row;
-            while ((row = csvReader.readNext()) != null) {
-                rows.add(row);
-            }
-        } catch (IOException | CsvValidationException e) {
-            throw new Exception("Error parsing data file.", e);
-        }
-
-        return rows;
     }
 }
